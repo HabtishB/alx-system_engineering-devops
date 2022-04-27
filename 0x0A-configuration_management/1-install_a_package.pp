198eb53be026333b8e823f751ddd003ad5558eb0
HabtishB <behabtm@gmail.com>
2022-04-27 10:42:33 -0700

    1 modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
 # installs flask from pip3
-include python
-include python::flask 
+
 package { 'Flask 2.1.0':
   ensure   => '2.1.0',
-  name     => 'flask'
+  name     => 'flask',
   provider => 'pip3',
-  require  => Class['python::flask'],
 }
